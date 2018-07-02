00eeae7d170c4feed6e596b8ad6de797ec417530
gerardog17 <pacoescobedo17@gmail.com>
2018-07-01 22:15:37 -0500

    se termino ejercicio de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-    import { Component, OnInit } from '@angular/core';
-    import { Hero } from '../hero';
-     
-    @Component({
-      selector: 'app-heroes',
-      templateUrl: './heroes.component.html',
-      styleUrls: ['./heroes.component.css']
-    })
-    export class HeroesComponent implements OnInit {
-      hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-      };
-     
-      constructor() { }
-     
-      ngOnInit() {
-      }
-     
-    }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
